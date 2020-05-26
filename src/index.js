5d8058e150a3da7f1b2d10e4f70fd6f7be189603
Alex <alexgomeznear@gmail.com>
2020-05-26 00:14:22 -0500

    dontenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV !== 'production'){// si no estoy en produccion voy a utilizar ...process.env.NODE_ENV nos da el entorno actual del proyecto (dev o produccion)
 
-   require('dotenv').config(); // lo que hace es que si existe un archivo al inicio del proyecto llamdo .env va a leer lo que tiene dentro y lo asignara a las variables de entorno
+   const dotenv = require('dotenv');
+   dotenv.config(); // lo que hace es que si existe un archivo al inicio del proyecto llamdo .env va a leer lo que tiene dentro y lo asignara a las variables de entorno
    //console.log(process.env.TESTING); //process.env es una variable que tiene acceso al sistema
 }
 
