be7c0056a219364b785715492480921ecf1b00ff
Alex <alexgomeznear@gmail.com>
2020-05-26 01:03:33 -0500

    Listo para Produccion Ahora si

--- src/controllers/categorias.controller.js
+++ src/controllers/categorias.controller.js
@@ -5,9 +5,9 @@ const Producto = require('../models/Producto');
 
 categoriasCtrl.renderCategoria1 = async (req,res) =>{
    const productos = await Producto.find({"categoria": "Equipo medico"}).sort({createdAt:'desc'}); //Hacemos una peticion a la bd, lo filtramos segun si el usuario le pertenece y lo ordenamos por fecha de manera desendente
-   //var n = await Producto.count({"categoria": "Equipo medico"});//Algoritmo aleatorio
+   var n = await Producto.count({"categoria": "Equipo medico"});//Algoritmo aleatorio
    var r = Math.floor(Math.random() * n);
-   const productosA = await Producto.find({"categoria": "Equipo medico"}).limit(3);
+   const productosA = await Producto.find({"categoria": "Equipo medico"}).limit(3).skip(r);
 
    res.render('categorias/equipoMedico' ,{productos,productosA});
 };
@@ -15,27 +15,27 @@ categoriasCtrl.renderCategoria1 = async (req,res) =>{
 categoriasCtrl.renderCategoria2 = async (req,res) =>{
    const productos = await Producto.find({"categoria": "Medio de contraste"}).sort({createdAt:'desc'}); //Hacemos una peticion a la bd, lo filtramos segun si el usuario le pertenece y lo ordenamos por fecha de manera desendent
 
-   //var n = await Producto.count({"categoria": "Medio de contraste"});//Algoritmo aleatorio
+   var n = await Producto.count({"categoria": "Medio de contraste"});//Algoritmo aleatorio
    var r = Math.floor(Math.random() * n);
-   const productosA = await Producto.find({"categoria": "Medio de contraste"}).limit(3);
+   const productosA = await Producto.find({"categoria": "Medio de contraste"}).limit(3).skip(r);
 
    res.render('categorias/medioDeContraste', {productos,productosA});
 };
 
 categoriasCtrl.renderCategoria3 = async (req,res) =>{
    const productos = await Producto.find({"categoria": "Material radiologico"}).sort({createdAt:'desc'}); //Hacemos una peticion a la bd, lo filtramos segun si el usuario le pertenece y lo ordenamos por fecha de manera desendente
-   //var n = await Producto.count({"categoria": "Material radiologico"});//Algoritmo aleatorio
+   var n = await Producto.count({"categoria": "Material radiologico"});//Algoritmo aleatorio
    var r = Math.floor(Math.random() * n);
-   const productosA = await Producto.find({"categoria": "Material radiologico"}).limit(3);
+   const productosA = await Producto.find({"categoria": "Material radiologico"}).limit(3).skip(r);
 
    res.render('categorias/materialRadiologico', {productos,productosA});
 };
 
 categoriasCtrl.renderCategoria4 = async (req,res) =>{
    const productos = await Producto.find({"categoria": "Material de curacion"}).sort({createdAt:'desc'});
-   //var n = await Producto.count({"categoria": "Material de curacion"});//Algoritmo aleatorio
+   var n = await Producto.count({"categoria": "Material de curacion"});//Algoritmo aleatorio
    var r = Math.floor(Math.random() * n);
-   const productosA = await Producto.find({"categoria": "Material de curacion"}).limit(3);
+   const productosA = await Producto.find({"categoria": "Material de curacion"}).limit(3).skip(r);
 
    res.render('categorias/materialDeCuracion', {productos,productosA});
 }
